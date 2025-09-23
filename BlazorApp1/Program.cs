3a3314ecfc76c7a803723c526034b12bcd07b84e
Kishan Hajari <hajarikishan02@outlook.com>
2025-09-23 14:54:23 +0530

    JWT and Dashboard Removed.

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,29 +1,28 @@
-﻿using Diamond.WebUI.Components;
+using Diamond.WebUI.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-       .AddInteractiveServerComponents();
-
-builder.Services.AddHttpClient();
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
+
 
 app.UseAntiforgery();
 
+app.MapStaticAssets();
 app.MapRazorComponents<App>()
-   .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode();
 
 app.Run();
--- Diamond.API/Program.cs
+++ Diamond.API/Program.cs
@@ -6,8 +6,6 @@ using Diamond.API.Repositories.Polish;
 using Diamond.API.Repositories.Purity;
 using Diamond.API.Repositories.ShapeRepository;
 using Diamond.API.Repositories.Shapes;
-using Diamond.API.Repositories;
-using Diamond.API.Services;
 using Diamond.API.Services.Clarity;
 using Diamond.API.Services.Colors;
 using Diamond.API.Services.Cut;
@@ -16,9 +14,6 @@ using Diamond.API.Services.Purity;
 using Diamond.API.Services.Shapes;
 using Microsoft.Data.SqlClient;
 using System.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,7 +34,6 @@ builder.Services.AddScoped<IClarityRepository, ClarityRepository>();
 builder.Services.AddScoped<ICutRepository, CutRepository>();
 builder.Services.AddScoped<IPurityRepository, PurityRepository>();
 builder.Services.AddScoped<IPolishRepository, PolishRepository>();
-builder.Services.AddScoped<UserRepository>();
 
 // Services
 builder.Services.AddScoped<IColorService, ColorService>();
@@ -48,32 +42,6 @@ builder.Services.AddScoped<IClarityService, ClarityService>();
 builder.Services.AddScoped<ICutService, CutService>();
 builder.Services.AddScoped<IPurityService, PurityService>();
 builder.Services.AddScoped<IPolishService, PolishService>();
-//builder.Services.AddScoped<AuthService>();
-
-// JWT auth
-//var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
-//builder.Services.AddAuthentication(options =>
-//{
-//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-//})
-//.AddJwtBearer(options =>
-//{
-//    options.RequireHttpsMetadata = false; // set true in prod
-//    options.SaveToken = true;
-//    options.TokenValidationParameters = new TokenValidationParameters
-//    {
-//        ValidateIssuer = true,
-//        ValidateAudience = true,
-//        ValidateLifetime = true,
-//        ValidateIssuerSigningKey = true,
-//        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-//        ValidAudience = builder.Configuration["Jwt:Audience"],
-//        IssuerSigningKey = new SymmetricSecurityKey(key)
-//    };
-//});
-
-//builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -84,11 +52,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.MapControllers();
 
